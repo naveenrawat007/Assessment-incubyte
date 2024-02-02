572ca54c77c9edd37dc4c68788f59410030c9dd4
naveen rawat <naveenrawat808@gmail.com>
2024-02-02 15:25:32 +0530

    add test spec for move forward & move backward method

--- chandrayaan_spec.rb
+++ chandrayaan_spec.rb
@@ -14,6 +14,22 @@ describe Chandrayaan3 do
 		expect(spacecraft.execute_commands(['f','b','l']))
 		end
 	end
+
+	describe '#move_forward' do
+    it 'moves the spacecraft one step forward based on its current direction' do
+      spacecraft = Chandrayaan3.new(0, 0, 0, 'N')
+      spacecraft.move_forward
+      expect(spacecraft.position).to eq([0, 1, 0])
+    end
+  end
+
+	describe '#move_backward' do
+    it 'moves the spacecraft one step backward based on its current direction' do
+      spacecraft = Chandrayaan3.new(0, 0, 0, 'N')
+      spacecraft.move_forward
+      expect(spacecraft.position).to eq([0, -1, 0])
+    end
+  end
 	
 
 
