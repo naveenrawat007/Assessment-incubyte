6ef01c5c4ccc985f54d87128bc92453c9eecba4d
naveen rawat <naveenrawat808@gmail.com>
2024-02-02 14:51:44 +0530

    add spec for execute command method

--- chandrayaan_spec.rb
+++ chandrayaan_spec.rb
@@ -1,9 +1,20 @@
 describe Chandrayaan3 do
-    describe '#initialize' do
-      it 'initializes the spacecraft with the given starting position and direction' do
-        spacecraft = Chandrayaan3.new(0, 0, 0, 'N')
-        expect(spacecraft.position).to eq([0, 0, 0])
-        expect(spacecraft.direction).to eq('N')
-      end
-    end
-  end
+
+	describe '#initialize' do
+		it 'initializes the spacecraft with the given starting position and direction' do
+		spacecraft = Chandrayaan3.new(0, 0, 0, 'N')
+		expect(spacecraft.position).to eq([0, 0, 0])
+		expect(spacecraft.direction).to eq('N')
+		end
+	end
+
+	describe '#execute_commands' do
+		it 'will execute the corrent method as per command' do
+		spacecraft = Chandrayaan3.new(0, 0, 0, 'N')
+		expect(spacecraft.execute_commands(['f','b','l']))
+		end
+	end
+	
+
+
+end
