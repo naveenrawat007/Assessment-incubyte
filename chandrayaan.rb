8a5d57da62618c8aacac790a10080bca79f980cc
naveen rawat <naveenrawat808@gmail.com>
2024-02-02 15:11:55 +0530

    add move forward and move backward methods

--- chandrayaan.rb
+++ chandrayaan.rb
@@ -25,4 +25,40 @@ class Chandrayaan3
 		end
 	end
 
+	private
+
+	def move_forward
+    case @direction
+    when 'N'
+      @position[1] += 1
+    when 'S'
+      @position[1] -= 1
+    when 'E'
+      @position[0] += 1
+    when 'W'
+      @position[0] -= 1
+    when 'Up'
+      @position[2] += 1
+    when 'Down'
+      @position[2] -= 1
+    end
+  end
+
+	def move_backward
+    case @direction
+    when 'N'
+      @position[1] -= 1
+    when 'S'
+      @position[1] += 1
+    when 'E'
+      @position[0] -= 1
+    when 'W'
+      @position[0] += 1
+    when 'Up'
+      @position[2] -= 1
+    when 'Down'
+      @position[2] += 1
+    end
+  end
+
 end
