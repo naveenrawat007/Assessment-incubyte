58634a425d740b0f28fd18ed2b41156f9f60ae92
naveen rawat <naveenrawat808@gmail.com>
2024-02-02 14:43:48 +0530

    add execute commands method

--- chandrayaan.rb
+++ chandrayaan.rb
@@ -1,9 +1,28 @@
 class Chandrayaan3
-    attr_accessor :position, :direction
-  
-    def initialize(x, y, z, direction)
-      @position = [x, y, z]
-      @direction = direction
-    end
+	attr_accessor :position, :direction
+
+	def initialize(x, y, z, direction)
+		@position = [x, y, z]
+		@direction = direction
+	end
+
+	def execute_commands(commands)
+		commands.each do |command|
+			case command
+			when 'f'
+				move_forward
+			when 'b'
+				move_backward
+			when 'l'
+				turn_left
+			when 'r'
+				turn_right
+			when 'u'
+				turn_up
+			when 'd'
+				turn_down
+			end
+		end
+	end
 
 end
